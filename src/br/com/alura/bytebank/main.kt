bd19189256221e7f951d93bacf1cef8c89580062
Alex Felipe <alexfelipevieira@gmail.com>
2020-04-21 11:15:59 -0300

    1.4

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,10 +1,26 @@
 package br.com.alura.bytebank
 
 fun main() {
-    val minhaFuncao = ::teste
-    println(minhaFuncao())
+//    testaTipoFuncaoReferencia()
+//    testaTipoFuncaoClasse()
+    val minhaFuncaoLambda = {
+        println("Executa como lambda")
+    }
+    println(minhaFuncaoLambda())
+    val minhaFuncaoAnonima: () -> Unit = fun(){
+        println("Executa como anonima")
+    }
+    println(minhaFuncaoAnonima())
+}
+
+fun testaTipoFuncaoClasse() {
     val minhaFuncaoClasse: () -> Unit = Teste()
     println(minhaFuncaoClasse())
+}
+
+fun testaTipoFuncaoReferencia() {
+    val minhaFuncao = ::teste
+    println(minhaFuncao())
 }
 
 fun teste() {
