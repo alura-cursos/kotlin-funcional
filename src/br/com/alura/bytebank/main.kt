565c0e543c314672626808ad51dc225b8164a52d
Alex Felipe <alexfelipevieira@gmail.com>
2020-04-22 11:06:56 -0300

    2.3

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -12,6 +12,22 @@ fun main() {
         return a + b
     }
     println(minhaFuncaoAnonima(20, 10))
+
+    val calculaBonificacao: (salario: Double) -> Double = lambda@{ salario ->
+        if(salario > 1000.0){
+            return@lambda salario + 50
+        }
+        salario + 100.0
+    }
+    println(calculaBonificacao(1000.0))
+
+    val calculaBonificacaoAnonima: (salario: Double) -> Double = fun(salario): Double {
+        if(salario > 1000.0){
+            return salario + 50
+        }
+        return salario + 100.0
+    }
+    println(calculaBonificacaoAnonima(1100.0))
 }
 
 fun testaTipoFuncaoClasse() {
