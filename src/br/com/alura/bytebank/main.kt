5898345c9ae42c4886ac44c6607c024d946b6210
Alex Felipe <alexfelipevieira@gmail.com>
2020-04-22 08:47:41 -0300

    2.1

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,34 +1,32 @@
 package br.com.alura.bytebank
 
 fun main() {
-//    testaTipoFuncaoReferencia()
-//    testaTipoFuncaoClasse()
-    val minhaFuncaoLambda = {
-        println("Executa como lambda")
-    }
-    println(minhaFuncaoLambda())
-    val minhaFuncaoAnonima: () -> Unit = fun(){
-        println("Executa como anonima")
-    }
-    println(minhaFuncaoAnonima())
+    testaTipoFuncaoReferencia()
+    testaTipoFuncaoClasse()
+//    val minhaFuncaoLambda = {
+//        println("Executa como lambda")
+//    }
+//    println(minhaFuncaoLambda())
+//    val minhaFuncaoAnonima: () -> Unit = fun(){
+//        println("Executa como anonima")
+//    }
+//    println(minhaFuncaoAnonima())
 }
 
 fun testaTipoFuncaoClasse() {
-    val minhaFuncaoClasse: () -> Unit = Teste()
-    println(minhaFuncaoClasse())
+    val minhaFuncaoClasse: (Int, Int) -> Int = Soma()
+    println(minhaFuncaoClasse(10, 10))
 }
 
 fun testaTipoFuncaoReferencia() {
-    val minhaFuncao = ::teste
-    println(minhaFuncao())
+    val minhaFuncao: (Int, Int) -> Int = ::soma
+    println(minhaFuncao(5, 10))
 }
 
-fun teste() {
-    println("executa teste")
+fun soma(a: Int, b: Int) : Int {
+    return a + b
 }
 
-class Teste : () -> Unit {
-    override fun invoke() {
-        println("executa invoke do Teste")
-    }
+class Soma : (Int, Int) -> Int {
+    override fun invoke(a: Int, b: Int): Int = a + b
 }
