b86f21db70bb082d0c15927bf56d116462913e75
Alex Felipe <alexfelipevieira@gmail.com>
2020-04-27 09:05:22 -0300

    4.2

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -10,9 +10,11 @@ fun main() {
 //    println(enderecoEmMaiusculo)
 
     Endereco(logradouro = "rua vergueiro", numero = 3185)
-        .apply {
+        .run {
             "$logradouro, $numero".toUpperCase()
-        }.let(::println)
+        }.let {enderecoEmMaiusculo: String ->
+            println(enderecoEmMaiusculo)
+        }
 
     listOf(Endereco(complemento = "casa"),
         Endereco(),
